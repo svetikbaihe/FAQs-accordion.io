b2d5cc747fb56a0f47c20c70a6373700af8ca85f
Світлана Ем <svitlanaem@MacBook-Air-Svitlana.local>
2024-04-21 20:58:05 +0900

    feat:faqs svg update

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -36,7 +36,7 @@ class Accordion {
     $spanText.innerText = `${this.question}`;
 
     const $buttonSvg = document.createElement('img');
-    $buttonSvg.setAttribute('src', './src/assets/svg/icon-plus.svg');
+    $buttonSvg.setAttribute('src', "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='30' height='31' fill='none' viewBox='0 0 30 31'%3E%3Cpath fill='%23AD28EB' d='M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z'/%3E%3C/svg%3E");
     $buttonSvg.setAttribute('id', styles['buttonSvg']);
 
     $questionBtn.appendChild($spanText);
@@ -45,10 +45,10 @@ class Accordion {
     $questionBtn.addEventListener('click', () => {
       if (!this.accordion.children[1]) {
         this.accordion.appendChild(this.buildAnswerWrapper());
-        $buttonSvg.setAttribute('src', './src/assets/svg/icon-minus.svg');
+        $buttonSvg.setAttribute('src', "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='30' height='31' fill='none' viewBox='0 0 30 31'%3E%3Cpath fill='%23301534' d='M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z'/%3E%3C/svg%3E");
       } else {
         this.accordion.children[1].remove();
-        $buttonSvg.setAttribute('src', './src/assets/svg/icon-plus.svg');
+        $buttonSvg.setAttribute('src', "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='30' height='31' fill='none' viewBox='0 0 30 31'%3E%3Cpath fill='%23AD28EB' d='M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z'/%3E%3C/svg%3E");
       }
     })
 
